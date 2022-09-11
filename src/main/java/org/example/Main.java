4d4b2fb24aa8ba9351b656638e998e4175ac586a
Dmytro Svirniuk <dmytro123svi@gmail.com>
2022-09-11 22:28:33 +0300

    All comments have been corrected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import org.example.model.Pair;
+
+import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.Scanner;
 import java.util.Set;
+
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toCollection;
 
 public class Main {
     public String arrayChallenge(int[] arr) {
+        if (arr.length == 0){
+            return "array is empty";
+        }
         Set<Pair> pairs = new LinkedHashSet<>();
+
         for (int i = 0; i < arr.length; i += 2) {
             pairs.add(new Pair(arr[i], arr[i + 1]));
         }
@@ -23,8 +32,14 @@ public class Main {
         return set.stream().map(Pair::toString).collect(joining(", "));
     }
     public static void main(String[] args) {
-        int[] arr = {2,1,1,2,3,3};
-      //  int[] arr = {5,4,6,7,7,6,4,5};
+        Scanner scan = new Scanner(System.in);
+        int[] arr = Arrays.stream(scan.nextLine()
+                        .trim()
+                        .split(","))
+                .filter(x ->  !x.equals(""))
+                .mapToInt(Integer::parseInt)
+                .filter(x -> x >= 0)
+                .toArray();
         System.out.println(new Main().arrayChallenge(arr));
     }
 }
